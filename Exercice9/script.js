67e516b8f388b7dd803540cc68b9ad5069b31c7c
Alexi GRIGOROV <alexigrigorov@gmail.com>
2024-07-08 12:13:04 +0200

    finished the tests (OC bugs in 4 and 6?)

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,3 +1,12 @@
 // votre code ici
+function checkAge(age) {
+  if (age < 18) {
+    return `Vous êtes mineur.`;
+  }
+  if (age >= 65) {
+    return `Vous êtes senior.`;
+  }
+  return `Vous êtes majeur.`;
+}
 
 export default checkAge;
